ad7ddf803b127e2583dc687889aa3cb92e047dc7
Mike Burns <mgburns@bu.edu>
2014-04-01 14:50:49 -0400

    Make dummy grader return a proper response

--- tests/modules/evaluators/dummy.py
+++ tests/modules/evaluators/dummy.py
@@ -5,4 +5,4 @@ class DummyEvaluator(BaseEvaluator):
     name = 'dummy'
 
     def evaluate(self, submission):
-        return True
+        return {"correct": True, "score": 1, "msg": "<p>Nice Work!</p>"}
