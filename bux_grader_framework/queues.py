f3abb8d0c64a4c3636330d6ef87b1c39bc18b97f
Mike Burns <mgburns@bu.edu>
2014-04-01 10:14:52 -0400

    Adds temporary WorkQueue implementation using multiprocessing.Queue

--- bux_grader_framework/queues.py
+++ bux_grader_framework/queues.py
@@ -1,25 +1,46 @@
+import logging
+import time
+
+import multiprocessing
+import Queue
+
+log = logging.getLogger(__name__)
+
 
 class WorkQueue(object):
     """ Internal submission work queue.
 
-    Thin abstraction around RabbitMQ.
+    First pass is a thin wrapper around multiprocessing.Queue.
+
+    Note that the "queue_name" parameter that will be used to route submissions
+    to specific evaluators in the final version is ignored. (A single shared
+    queue is used for simplicity).
 
     """
-    def __init__(self, host, port, username, password, virtual_host):
-        pass
-
-    def connect(self):
-        """ Establish connection to message broker """
-        pass
+    queue = multiprocessing.Queue()
 
     def get(self, queue_name):
         """ Pop a submission off the work queue """
-        pass
+        try:
+            submission = self.queue.get(False)
+        except Queue.Empty:
+            return None
+        else:
+            log.info(" < Popped submission #%d off of '%s' queue",
+                     submission["xqueue_header"]["submission_id"], queue_name)
+            return submission
 
-    def put(self, queue_name, msg):
+    def put(self, queue_name, submission):
         """ Push a submission on to the work queue """
-        pass
+        log.info(" > Put result for submisson #%d to '%s' queue",
+                 submission["xqueue_header"]["submission_id"], queue_name)
+        self.queue.put(submission)
 
-    def consume(self, queue_name):
+    def consume(self, queue_name, handler):
         """ Poll a particular queue for submissions """
-        pass
+        while True:
+            submission = self.get(queue_name)
+            if submission:
+                handler(submission)
+            else:
+                time.sleep(1)
