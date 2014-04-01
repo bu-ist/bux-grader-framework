8be395038dd1772334298d99c723b4a3a345ae6e
Mike Burns <mgburns@bu.edu>
2014-04-01 14:27:45 -0400

    Adds a RabbitMQ-backed work queue.

--- bux_grader_framework/queues.py
+++ bux_grader_framework/queues.py
@@ -1,8 +1,10 @@
+import json
 import logging
 import time
-
 import multiprocessing
 import Queue
+
+import pika
 
 log = logging.getLogger(__name__)
 
@@ -44,3 +46,94 @@ class WorkQueue(object):
                 handler(submission)
             else:
                 time.sleep(1)
+
+
+class RabbitMQueue(object):
+    """ Internal submission work queue. Backed by RabbitMQ. """
+    def __init__(self, username='guest', password='guest', host='localhost',
+                 port=5672, virtual_host='/'):
+        self.username = username
+        self.password = password
+        self.host = host
+        self.port = port
+        self.virtual_host = virtual_host
+
+        credentials = pika.PlainCredentials(self.username,
+                                            self.password)
+        self.params = pika.ConnectionParameters(host=self.host,
+                                                port=self.port,
+                                                virtual_host=self.virtual_host,
+                                                credentials=credentials)
+        self._connection = None
+        self._channel = None
+
+    def connect(self):
+        """ Establish a connection to RabbitMQ """
+        self._connection = pika.BlockingConnection(self.params)
+
+    def get_channel(self):
+        """ Returns a connection channel for RabbitMQ """
+        if not self._connection or self._connection.is_closed:
+            self.connect()
+
+        if not self._channel or self._channel.is_closed:
+            self._channel = self._connection.channel()
+
+        return self._channel
+
+    def put(self, queue_name, submission):
+        """ Push a submission on to the work queue """
+        log.info(" >> Enqueueing submisson #%d to '%s' queue",
+                 submission["xqueue_header"]["submission_id"], queue_name)
+
+        channel = self.get_channel()
+        channel.queue_declare(queue=queue_name, durable=True)
+
+        properties = pika.BasicProperties(content_type='application/json',
+                                          delivery_mode=2)
+
+        channel.basic_publish(exchange='',
+                              routing_key=queue_name,
+                              body=json.dumps(submission),
+                              properties=properties)
+
+    def consume(self, queue_name, eval_callback):
+        """ Poll a particular queue for submissions
+
+            :param str queue_name: queue to consume requests from
+            :param callable eval_callback: called when a submission is received
+
+        """
+
+        channel = self.get_channel()
+        channel.queue_declare(queue=queue_name, durable=True)
+
+        def on_message_received(ch, method, header, body):
+            """ Hides the RabbitMQ mechanics from the evaluator workers """
+            tag = method.delivery_tag
+            log.info(" << Message %d consumed: %s, %s, %s",
+                     tag, method, header, body)
+
+            submission = json.loads(body)
+            response = eval_callback(submission)
+            if response:
+                log.info(u" \u2713 Message %d acknowledged!", tag)
+                ch.basic_ack(delivery_tag=tag)
+            else:
+                log.error(u" \u2715 Message %d could not be evaluated: %s",
+                          tag, submission)
+
+                # TODO: Establish a procedure for recovering failed submissions
+                ch.basic_nack(delivery_tag=tag, requeue=False)
+
+        channel.basic_consume(on_message_received, queue_name)
+
+        try:
+            channel.start_consuming()
+        except KeyboardInterrupt:
+            channel.stop_consuming()
+            raise
+
+    def close(self):
+        if self._connection:
+            self._connection.close()
