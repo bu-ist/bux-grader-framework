59466fe4acd9060e30610d03312b6b77a148d70a
Mike Burns <mgburns@bu.edu>
2014-04-01 14:59:54 -0400

    Updates test config modules to reflect new format

--- tests/modules/dummy_config.py
+++ tests/modules/dummy_config.py
@@ -2,16 +2,24 @@
 #
 # Loads the DummyEvaluator class
 
-XQUEUE_INTERFACE = {
-    "queue": "test_queue",
-    "url": "http://localhost:18040",
-    "username": "lms",
-    "password": "password",
-    "timeout": 10
-}
+XQUEUE_QUEUE = "test_queue"
+XQUEUE_URL = "http://localhost:18040"
+XQUEUE_USER = "lms"
+XQUEUE_PASSWORD = "password"
+XQUEUE_TIMEOUT = 10
 
-ignoreme = "test"
+RABBITMQ_USER = "guest"
+RABBITMQ_PASSWORD = "guest"
+RABBITMQ_HOST = "localhost"
+RABBITMQ_PORT = 5672
+RABBITMQ_VHOST = "/"
+
+WORKER_COUNT = 2
+
+MONITOR_INTERVAL = 1
 
 EVALUATOR_MODULES = {
     'evaluators.dummy'
 }
+
+ignoreme = "test"
--- tests/modules/missing_config.py
+++ tests/modules/missing_config.py
@@ -2,12 +2,18 @@
 #
 # Missing EVALUATOR_MODULES
 
-XQUEUE_INTERFACE = {
-    "queue": "test_queue",
-    "url": "http://localhost:18040",
-    "username": "lms",
-    "password": "password",
-    "timeout": 10
-}
+XQUEUE_QUEUE = "test_queue"
+XQUEUE_URL = "http://localhost:18040"
+XQUEUE_USER = "lms"
+XQUEUE_PASSWORD = "password"
+XQUEUE_TIMEOUT = 10
 
-ignoreme = "test"
+RABBITMQ_USER = "guest"
+RABBITMQ_PASSWORD = "guest"
+RABBITMQ_HOST = "localhost"
+RABBITMQ_PORT = 5672
+RABBITMQ_VHOST = "/"
+
+WORKER_COUNT = 2
+
+MONITOR_INTERVAL = 1
